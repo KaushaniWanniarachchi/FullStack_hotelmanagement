c8013d46c2f6fe869af341da1b03154228dba246
KAUSHANI <79128689+KaushaniWanniarachchi@users.noreply.github.com>
2023-01-25 18:37:51 +0530

    commit

--- server/config/express.js
+++ server/config/express.js
@@ -33,15 +33,10 @@ app.use(helmet());
 
 app.use(cors());
 
-// authenticate
+
 app.use(passport.initialize());
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-//     res.header("Access-Control-Allow-Methods: PUT,GET,POST,DELETE");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");    
-//     next();
-// });
+
 const httpServer = require('http').createServer(app);
 const io = require('socket.io')(httpServer, {
   cors: {origin : '*'}
@@ -59,19 +54,19 @@ io.on('connection', (socket) => {
   });
 });
 
-// api router
+
 app.use('/api/', routes);
 
-// serve th index.html
+
 app.get('*', (req, res) => res.sendFile(path.join(distDir, 'index.html')));
 
-// catch the 404 and forward to error handler
+
 app.use((req, res, next) => {
     const error = new HttpError(404);
     return next(error);
 });
 
-// error handler, stack trace
+
 app.use((err, req, res, next) => {
     res.status(err.status || 500).json({
         message: err.message
