24107705e8a7fae92693bdc3ec7e53eaac4613a3
KAUSHANI <79128689+KaushaniWanniarachchi@users.noreply.github.com>
2023-01-25 18:35:05 +0530

    commit

--- server/config/express.js
+++ server/config/express.js
@@ -9,28 +9,28 @@ const routes = require('../routes');
 const passport = require('../middleware/passport');
 const HttpError = require('http-errors');
 
-// get app
+
 const app = express();
 
-// logger
+
 if(config.env==='development') {
     app.use(logger('dev'));
 }
 
-// get dist folder
+
 const distDir = path.join(__dirname, '../../dist')
 
-// use dist folder as hosting by express
+
 app.use(express.static(distDir));
 
-// parsing from api
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// secure apps http
+
 app.use(helmet());
 
-// allow cors
+
 app.use(cors());
 
 // authenticate
